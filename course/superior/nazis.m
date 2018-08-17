48eaeb68b091fbfe0ffa36add05705dff3f2f614
krollo1 <42063781+krollo1@users.noreply.github.com>
2018-08-17 12:25:14 +0100

    Create nazis.m

---
+++ course/superior/nazis.m
@@ -0,0 +1,46 @@
+---
+Title: Mathematics in Nazi Germany
+Template: LeafPage
+---
+
+---
+title: Nazis
+Template: LeafPage
+---
+
+**The case of Albert Speer**
+
+As a young man, Albert Speer was deeply entranced by mathematics; in an anecdote from one of his later interviews, he recalled how in his final Abitur exams, he started revising three days before they started, only to complete the maths papers incredibly quickly and reattack each problem from a different perspective – entirely for his own fun, he claimed. After studying architecture at university – his father claimed that he would never earn much as a mathematician – he became a quietly respectable professional for a few years.  
+
+After joining the Nazi Party in 1931, he rapidly rose through the ranks, at first principally acting as an architect for Hitler, designing the new grand monuments of Nazi Germany. Few were constructed, and fewer survive. His role changed substantially, however, in 1942, when he was appointed to Minister of Armaments and War Production. This put him in a position utterly integral to the German war effort; he enacted his duties with gusto, taking deep control over factories on an individual basis, and rendering the German industrial setup considerably more efficient.
+
+He was not completely detached from the reality of the Reich, however. On several factory visits, in particular to the underground V2 rocket factory, he was witnessed what he later realised was concentration camp labour. In the final days of the war, he was one of the only senior Nazis to seriously stand up to Hitler, refusing to implement his plan to destroy German infrastructure to slow the progress of the Allies; in this regard he was the first senior Nazi to recognise that the war was lost. 
+
+After the war, he became known as the 'Nazi who said sorry' after, either cannily or genuinely, proclaiming his individual responsibility for the Nazi regime's actions while simultaneously claiming he had never known about the horrors of the Holocaust. Perhaps he was telling the truth, perhaps not. We shall probably never know. But even if he had known – even if had been told exactly what was going on – what could he have done? It was entirely infeasible to leave the War Cabinet without putting himself in colossal personal danger, and he had insufficient authority to meaningfully stop the Holocaust by himself. It was much easier, and really the only feasible option, to construct his own reality, to constantly think that these horrible insinuations were never quite as bad as they seemed – in short, to keep doing the day job without asking too many questions.
+
+After the war, he spent some twenty years in Spandau before being released, publishing several books about the inner workings of Nazi Germany, and generally being treated very well by the public.
+
+**Nazi mathematics**
+
+To what extent, however, was Speer using mathematics in his actions? There is little evidence that he was using any formal optimisation techniques to improve German performance, and the rapid improvement while Speer was in the role is rather down to the very low efficiency at the start which Speer brought to strong competence, rather than an act of real genius. Undoubtedly he was very good at his job, but it was probably not achieved via mathematical sophistication.
+
+Mathematical research, however, was still active under the Nazi regime, though it lost much of its former brilliance. Countless mathematicians fled Germany, while others were killed during the Holocaust; Max Dehn, the first person to solve one of Hilbert's 23 problems, was forced to emigrate from Norway to the USA (via the Trans-Siberian Railway) in 1940, while Felix Hausdorff, on failing to obtain a research post abroad, committed suicide in 1942 rather than be sent to a death camp.  When asked by the Nazi Minister of Education how mathematics was at Gottingen, Hilbert memorably replied that "there is no mathematics at Gottingen any more". Hilbert later became engaged in a major dispute with LEJ Brouwer (the namesake of the fixed-point theorem) concerning their respective ideas of the logical basis of mathematics, namely formalism and intuitionism. While Hilbert was a great supporter of Jewish mathematicians in Germany at the time, Brouwer went on to claim superiority in the debate due to his being an 'Aryan German'. 
+
+I cite this disagreement to show how, even in matters of fairly abstruse, theoretical mathematics, politics, in certain conditions, can become inextricably intertwined with academic discourse. It is a poisonous tendency – but a tendency one must be aware of. All such men can become unobjective, even fanatical, in extreme political situations, however objective their mathematics seeks to be. Mathematics, on an academic level, must remain mathematics – for otherwise, the entire river of its development, from the Greeks downwards, can easily be dammed up. 
+
+I quote an exchange between GH Hardy and Ludwig Bieberbach (the founder of the Nazi \textit{Deutsche Mathematik} journal), which I think exemplifies both the warped worldview of the Nazi mathematician and that of the foreign mathematician in rejecting the former: 
+
+"In this way... the ultimate reason behind the courageous rejection which the students at Göttingen University meted out to a great mathematician, Edmund Landau, was that his un-German style in research and teaching had become intolerable to German sensitivities. A people which has seen how alien desires for dominion are gnawing at its identity, how enemies of the people are working to impose their alien ways on it, must reject teachers of a type alien to it."
+– Ludwig Bieberbach
+
+"There are many of us, many English and many Germans, who said things during the (First) War which we scarcely meant and are sorry to remember now. Anxiety for one's own position, dread of falling behind the rising torrent of folly, determination at all costs not to be outdone, may be natural if not particularly heroic excuses. Prof. Bieberbach's reputation excludes such explanation for his utterances; and I find myself driven to the more uncharitable conclusion that he really believes them true."
+– GH Hardy
+
+
+**Psychology**
+
+It is an old question: did the Nazi regime attract monsters, or did it produce them? 
+
+Particularly, Speer and Goebbels seem to belong to a similar archetype: both were brilliant at their specialisms, both were professionally unsuccessful, both were essentially apolitical until joining the Nazis, whereupon both seemed to find their calling. Suddenly in a position where they could attain great satisfaction through their work on a colossal scale, they became hardened Nazis almost through osmosis. Almost, it seems, they were Nazis because they could take up these opportunities, rather than taking them up because they were Nazis.
+
+It is perhaps a stretch, but it seems to me that this can be analogised to many mathematicians in industry the world over. There is an extent, especially in the finance industry, to which mathematicians are recruited and set to work on apparently pure, abstract problems. Either through ignorance or through wilful mental blocking, the mathematicians fail to consciously recognise the impact their work is actually having. As such, even when they are in an ethically problematic position, they are so inured to it all, so deep within a team, so taken in by their mathematical work that they are unable and unwilling to act upon it. A hyperbolic comparison in some ways, definitely, but the underlying mechanism is ever and always the same.   
